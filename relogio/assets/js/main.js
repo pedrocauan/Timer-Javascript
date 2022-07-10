fca46774f256db5e84a71f5c351fcb9a97304b77
pedrocauan <mainyasuo92@gmail.com>
2022-07-10 13:34:07 -0300

    Rodando timer

--- relogio/assets/js/main.js
+++ relogio/assets/js/main.js
@@ -5,22 +5,36 @@ const btnIniciar = document.querySelector(".iniciar")
 const btnPausar = document.querySelector(".pausar")
 
 const btnZerar = document.querySelector(".zerar")
-let segundos = 0
-const data = new Date(0)
+let segundos = 0 //Segundos contados
+let timer
+
+//cria a hora formatada com os segundos que se passaram
+const createHour = function() {
+  const data = new Date(segundos * 1000) //Converte de segundos para milisegundos
+  return data.toLocaleTimeString("pt-BR", {
+    hour12: false,
+    timeZone: "UTC"
+  })
+}
+
+ //Roda o relogio
+const init = function() {
+   //guarda a hora no timer 
+   timer = setInterval(() =>{
+    segundos++ //Atualiza a variavel segundos
+    relogio.innerHTML =  createHour(segundos)
+  }, 1000)
+}
 
 
 //Botao iniciar
 btnIniciar.addEventListener("click", function() {
-  //Roda o relogio
-  setInterval(() =>{
-    segundos++ //Atualiza a variavel segundos
-    relogio.innerHTML = segundos
-  }, 1000)
+  init()
 })
 
 //Botao de pausar
 btnPausar.addEventListener("click", function(){
-  alert("Pausar")
+  clearInterval(timer)
 })
 
 
